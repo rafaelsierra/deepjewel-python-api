0bddf151b03baa17a53c2ec1a237caeb04f23bac
Rafael Sierra <rafaeljsg14@gmail.com>
2011-12-01 23:45:36 -0200

    Added basic tests and a note about this WIP

--- README.rst
+++ README.rst
@@ -1,6 +1,9 @@
 DeepJewel API - Python Version
 ==============================
 
+*NOTE* - This a Work In Progress, which means everything could be (and probably are) broken or incomplete
+
+
 This is the pure-python version of DeepJewel API.
 
 DeepJewel is a recommendation system (currently closed alpha) in which you can just send your contents, whatever they are (news, products, blog post, even your users), and based on what some history, like user feeds/timelines/behavior, others products or news, you get customized recommendations.
---
+++ tests.py
@@ -0,0 +1,56 @@
+import unittest
+import deepapi
+import time
+import sys
+
+class MissingKeyException(Exception):
+    pass
+
+class TestSendData(unittest.TestCase):
+    def test_01_key(self):
+        # To run the tests we need a valid key
+        try:
+            self.key = raw_input('Enter your DeepJewel Key: ')
+        except IndexError, e:
+            raise MissingKeyException('You must run the test with an extra argument: Your DeepJewel Key')
+
+    def test_01_send_data(self):
+        # This is your product.id or post.id or user.screen_name or whatever 
+        # you think is useful, anything up to 500 chars
+        your_data = '1234' 
+        text = '''this is what your item is compound of, the bigger the text, 
+the better, currently the max text size is 10MB, which is pretty huge. And as
+ you can see, there's no need to do any parsing or anything like that, 
+everything will be parsed at the server side\nNew lines\ttabs, everything,
+ just send the raw text and everything will be ok'''
+        # Categories are a very useful functionality, it will keep your items
+        # into separated "boxes" so you can recommend only one type of item
+        #(like news or products)
+        # The category format is: lang|name
+        # In which lang options are: de, en, es, fr, it, pt. It is very important
+        # you set the correct language of your category, so DeepJewel can stem
+        # use a correct set of stopwords, increasing your recommendations relevance
+        category = 'en|post'
+        # Yet we have a endpoint to create categories, you can just send
+        # what is the category of this item and DeepJewel will create it
+        # if needed
+
+        # And then we send the text
+        created = deepapi.send(category, your_data, text)
+        # Simple like that :)
+        self.assertTrue(created)
+    
+    def test_02_recommend(self):
+        # It may take some seconds to flush data into database
+        time.sleep(5)
+        response = deepapi.recommend('en|post', '''Here again we send what text we want to
+base our recommendation, useful texts are user timeline, user feed, product description
+blog post and other relevant information''')
+        # Recommendations will always be relevant sorted, first result
+        # being the most relevant and last the least
+        
+        self.assertEqual(response[0]['your_data'], '1234')
+        
+
+if __name__ == '__main__':
+    unittest.main()
