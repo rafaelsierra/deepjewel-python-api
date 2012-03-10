d1b727baa67523ee4a1cb3032481a0d60d63ba19
Marcel Caraciolo <caraciol@gmail.com>
2012-03-10 10:47:44 -0300

    Pep8, modularity, docs and some adjustments at deepjewel api.

--- tests.py
+++ tests.py
@@ -3,29 +3,34 @@ import deepapi
 import time
 import sys
 
+
 class MissingKeyException(Exception):
     pass
 
+
 class TestSendData(unittest.TestCase):
-    def test_01(self): # Set key
+    def test_01(self):
+        # Set key
         # To run the tests we need a valid key
         try:
             self.key = sys.stdin.read().strip()
             if not self.key:
                 self.key = raw_input('Enter your DeepJewel Key: ')
             deepapi.set_key(self.key)
-        except IndexError, e:
-            raise MissingKeyException('You must run the test with an extra argument: Your DeepJewel Key')
+        except IndexError:
+            raise MissingKeyException('You must run the test with an ' +
+                                      'extra argument: Your DeepJewel Key')
 
-    def test_02(self): # Send Data
-        # This is your product.id or post.id or user.screen_name or whatever 
+    def test_send(self):
+        # Send Data
+        # This is your product.id or post.id or user.screen_name or whatever
         # you think is useful, anything up to 500 chars
-        your_data = '1234' 
-        text = '''this is what your item is compound of, the bigger the text, 
-the better, currently the max text size is 10MB, which is pretty huge. And as
- you can see, there's no need to do any parsing or anything like that, 
-everything will be parsed at the server side\nNew lines\ttabs, everything,
- just send the raw text and everything will be ok'''
+        your_data = '1234'
+        text = '''this is what your item is compound of, the bigger the text,
+        the better, currently the max text size is 10MB, which is pretty huge.
+        And as you can see, there's no need to do any parsing or anything like that,
+        everything will be parsed at the server side\nNew lines\ttabs, everything,
+        just send the raw text and everything will be ok'''
         # Categories are a very useful functionality, it will keep your items
         # into separated "boxes" so you can recommend only one type of item
         #(like news or products)
@@ -42,26 +47,28 @@ everything will be parsed at the server side\nNew lines\ttabs, everything,
         created = deepapi.send(category, your_data, text)
         # Simple like that :)
         self.assertTrue(created)
-       
-    def test_03(self): # Send user info
-        # Before get recommendations we need to inform DeepJewel about your 
+
+    def test_send_user_info(self):
+        # Send user info
+        # Before get recommendations we need to inform DeepJewel about your
         # user experiences, what he does, what he writes, the more information
         # the better will be his recommendations
-        
-        # Each call of deepapi.send_user_info will increase DeepJewel's 
+
+        # Each call of deepapi.send_user_info will increase DeepJewel's
         # knowledge about your user
         self.assertTrue(deepapi.send_user_info(42, 'Today I posted on my blog about tabs and new lines.'))
-        
+
         # Bit a bit DeepJewel understand better your user behavior
         self.assertTrue(deepapi.send_user_info(42, 'OMG! This was the biggest email I\'ve ever sent'))
-        
-        # Of course you can always reset your user behavior by sending 
+
+        # Of course you can always reset your user behavior by sending
         # add_info=False, then DeepJewel will forget everything you
         self.assertTrue(deepapi.send_user_info(24, 'BORA PRA FINAL DA LIBERTADORES SAO PAULO!!!', add_info=False))
-        # By this point, if your user (which we know for the ID 24) had any 
+        # By this point, if your user (which we know for the ID 24) had any
         # data within DeepJewel, it's already gone (and there's no way to take it back)
 
-    def test_04(self): # Recommend
+    def test_recommend(self):
+        # Recommend
         # It may take some seconds to flush data into database
         time.sleep(1)
         response = deepapi.recommend(42, 'en|post')
